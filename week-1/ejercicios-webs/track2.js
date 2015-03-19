90d96045f3f5d0fa4520875fd9df9660cc4d030a
Carles Nunez <carles.nunez@zitrointeractive.com>
2015-03-19 16:19:21 +0100

    Inicio del juego

---
+++ week-1/ejercicios-webs/track2.js
@@ -0,0 +1,14 @@
+//¡Asegúrate de que el usuario está listo para jugar!
+confirm("¿Estas listo para jugar?");
+var edad = prompt("¿Cuántos años tienes?");
+if(edad < 18){
+    console.log("Se te permite jugar bajo tu responsabilidad");    
+}else{
+    console.log("¡Empieza la aventura!");
+}
+
+console.log("Blancanieves y Batman estaban en el paradero de bus, esperando para poder ir de compras. Había una venta de liquidación, y ambos necesitaban comprar prendas nuevas. Batman nunca te cayó bien. Caminas hacia él.");
+
+console.log("Barman te mira con odio");
+
+var userAnswer = prompt("¿Te crees muy de buenas, idiota?");
