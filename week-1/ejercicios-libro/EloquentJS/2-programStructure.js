f47fb9b9a7ae8697d5c160bfd245766fd59fb0dd
Carles Nunez <carles.nunez@zitrointeractive.com>
2015-03-23 10:20:18 +0100

    Chapter 2, added three exercices

---
+++ week-1/ejercicios-libro/EloquentJS/2-programStructure.js
@@ -0,0 +1,57 @@
+//making a triangle
+function makeTriangle(){
+	for(var i = 0, output = ""; i < 7; i++){
+		output += "#"
+		console.log(output);
+	}
+}
+
+function printListOfNumbers(){
+	for(var i = 1; i <= 100; i++){
+	  if((i % 3 === 0) && (i % 5 === 0)){}else if((i % 3 === 0) && (i % 5 !== 0)){
+	  	console.log("FizzBuzz");
+	  }else if((i % 5 === 0) && (i % 3 !== 0)){
+	    console.log("Buzz")
+	  }else{
+	  	console.log(i);
+	  }
+	}
+}
+
+function chessTableMethod1(size){
+	// Your code here.
+	var line = "";
+	for(var i = 0; i < size;i++){
+	  line = "";
+	  for(var e = 0; e < (size/2);e++){
+	    if(i % 2 === 0){
+	  		line+= " #";
+	    }else{
+	      	line +="# ";
+	    }
+	  }
+	  console.log(line);
+	}
+}
+
+function chessTableMethod2(size){
+
+	// Your code here.
+	var line = "";
+	for(var i = 0; i < size;i++){
+	  line = "";
+	  for(var e = 0; e < size ;e++){
+	    if(i % 2 === 0 && e % 2 !== 0){
+	  		line+= "#";
+	    }else  if(i % 2 === 0 && e % 2 === 0){
+	    	line+= " ";
+	    }else if(i % 2 !== 0 && e % 2 !== 0){
+	      	line +=" ";
+	    }else{
+	    	line += "#";
+	    }
+	  }
+	  console.log(line);
+	}
+}
+chessTableMethod2(32);
