0111f7420ecf4126d66bde83fcc9a547cf1d15ce
Carles <carles.nunez@hotmail.es>
2015-08-04 13:15:59 +0200

    ()

---
+++ week-2/ejercicios-libro/EloquentJS/chapter7.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home</title>
+  </head>
+  <body>
+    <div class="homepage-wrapper">
+      <h2>Welcome to jQuery Travels - Traversing the DOM since 2006</h2>
+      <p>Fly to New York today for as little as <span>$299.99</span></p>
+    </div>
+    <script src="jquery.min.js"></script>
+    <script src="application.js"></script>
+  </body>
+</html>
+
+//
+
+<div id="vacations-wrapper">
+ <h1>Vacation Packages</h1>
+ <ul id="vacations">
+   <li class="vacation america">
+     <yes><h2>honoLulu</h2></yes>
+     <span class="details">$700</span>
+   </li>
+   <li class="vacation america">
+     <yes><h2>honoLulu</h2></yes>
+     <span class="details">$400</span>
+   </li>
+   <li class="vacation europe">
+     <yes><h2>honoLulu</h2></yes>
+     <span class="details">$1,100</span>
+   </li>
+   <li class="vacation asia">
+     <yes><h2>honoLulu</h2></yes>
+     <span class="details">$1,200</span>
+   </li>
+ </ul>
+  
+ <ul class="sorting">
+   <li><a href="#">America</a></li>
